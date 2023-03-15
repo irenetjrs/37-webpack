867589be567790bd7cadff3a85e6589ffc1de040
irenetjrs <irkapidgorodetska@gmail.com>
2023-03-15 22:21:19 +0200

    task 2

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
 const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
    entry: './src/index.js',
@@ -6,5 +9,22 @@ module.exports = {
    output: {
       path: path.resolve(__dirname, 'dist'),
       filename: '[name].[hash].js'
+   },
+   plugins: [
+      new HTMLWebpackPlugin({
+         template: './src/index.html'
+      }),
+      new CleanWebpackPlugin(),
+      new MiniCssExtractPlugin({
+         filename: '[name].[hash].css'
+      }),
+   ],
+   module:{
+      rules: [
+         {
+            test: /\.s[ac]ss$/,
+            use: [ MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+         },
+      ]
    },
 }
